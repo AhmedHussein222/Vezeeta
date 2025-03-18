b65ca77830b1769a9d318dfcceea8d585ca1d0b1
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-03-18 14:20:05 +0200

    Add Ahmed component with template, styles, and tests

---
+++ src/app/Component/ahmed/ahmed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ahmed',
+  imports: [],
+  templateUrl: './ahmed.component.html',
+  styleUrl: './ahmed.component.css'
+})
+export class AhmedComponent {
+
+}
